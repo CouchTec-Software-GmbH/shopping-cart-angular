000494953fe9e6c2db7de807bf15dc0b89264b6f
Linus Weigand <linusweigand@protonmail.com>
2024-01-22 14:31:43 +0100

    add cart component

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListingComponent } from '../product-listing/product-listing.component';
+import { Product } from '../productListing';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ProductListingComponent,
+  ],
+  template: `
+    <h2>Your Cart</h2>
+    <section class="cart">
+      <div *ngIf="cartProducts.length === 0">Your cart is empty.</div>
+      <app-product-listing
+        *ngFor="let product of cartProducts"
+        [product]="product">
+      </app-product-listing>
+    </section>
+  `,
+  styleUrl: './cart.component.css'
+})
+
+export class CartComponent {
+  cartProducts: Product[] = [];
+  productService: ProductService = inject(ProductService);
+
+  constructor() {
+    this.loadCartProducts();
+  }
+
+  async loadCartProducts() {
+    const cartIds: string[] = JSON.parse(localStorage.getItem('cart') || '[]');
+    this.cartProducts = [];
+
+    for (const id of cartIds) {
+      const product = await this.productService.getProductById(id);
+      if (product) {
+        this.cartProducts.push(product);
+      }
+    }
+  }
+}
