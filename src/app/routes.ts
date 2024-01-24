e335622f78509ffcb0d96cbbfdd1964a971ba888
Linus Weigand <linusweigand@protonmail.com>
2024-01-24 14:09:25 +0100

    add routes: configure & dashboard

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 import { DetailsComponent } from './details/details.component';
+import { ConfigureComponent } from './configure/configure.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routeConfig: Routes = [
   {
@@ -13,6 +15,15 @@ const routeConfig: Routes = [
     path: 'cart',
     component: CartComponent,
     title: 'Shopping Cart'
+  },
+  {
+    path: 'configure/:id',
+    component: ConfigureComponent,
+    title: 'Configure'
+  },{
+    path: 'dashboard',
+    component: DashboardComponent,
+    title: 'Dashboard'
   },
   {
     path: 'details/:id',
