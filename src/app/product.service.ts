3d4681303ec25b4e7d93ec0406c4afc172a7ce70
Linus Weigand <linusweigand@protonmail.com>
2024-02-27 17:50:27 +0100

    remove couchdb logic and pivot to backend

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,83 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Product } from './productListing';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  url = 'https://couchdb-app-service.azurewebsites.net/products/';
-  apiUrl = `https://couchtec.dev.linusweigand.de/api/create-user`;
-  private httpOptions: { headers: HttpHeaders };
 
+  // private productsUrl = 'https://couchtec.dev.linusweigand.de/api/products';
+  // private createUserUrl = 'https://couchtec.dev.linusweigand.de/api/create-user';
+  private url = 'http://localhost:80';
+  private productsUrl = `${this.url}/api/products`;
+  private createUserUrl = `${this.url}/api/create-user`;
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
-    const username = 'admin';
-    const password = '8RzuxhQ7';
+  constructor(private http: HttpClient) {}
 
-    this.httpOptions = {
-      headers: new HttpHeaders({
-        'Authorization': 'Basic ' + btoa(username + ':' + password),
-        'Content-Type': 'application/json'
-      })
-    };
-  }
-
+  // Fetch all products from the backend
   async getAllProducts(): Promise<Product[]> {
     try {
-      const response = await this.http.get<any>(`${this.url}_all_docs?include_docs=true`, this.httpOptions).toPromise();
-
-      const productPromises = response.rows.map(async (row: any, index: number) => {
-        const doc = row.doc;
-        let photoUrl = null;
-        if (doc._attachments) {
-          photoUrl = await this.getAttachmentUrl(doc._id, Object.keys(doc._attachments)[0]);
-        } else  {
-          photoUrl = null;
-        }
-        return {
-          id: index,
-          name: doc.name,
-          price: doc.price,
-          photo: photoUrl,
-          description: doc.description
-        } as Product;
+      const products = await this.http.get<Product[]>(this.productsUrl).toPromise() || [];
+      products.forEach(product => {
+        product.photo = this.constructPhotoUrl(product.id);
       });
-
-      return await Promise.all(productPromises);
+      return products;
     } catch (error) {
       console.error("Error fetching products: ", error);
       return [];
     }
   }
+
+  // Fetch a single product by ID from the backend
   async getProductById(id: string): Promise<Product | undefined> {
     try {
-      const response = await this.http.get<any>(`${this.url}${id}`, this.httpOptions).toPromise();
-
-      if (!response) {
-        return undefined;
+      const product = await this.http.get<Product>(`${this.productsUrl}/${id}`).toPromise();
+      if (product) {
+        product.photo = this.constructPhotoUrl(product.id);
       }
-
-      let photoUrl: string| SafeUrl = '';
-      if (response._attachments) {
-        photoUrl = await this.getAttachmentUrl(response._id, Object.keys(response._attachments)[0]);
-      }
-
-      return {
-        id: parseInt(id),
-        name: response.name,
-        price: response.price,
-        photo: photoUrl,
-        description: response.description
-      } as Product;
+      return product;
     } catch (error) {
       console.error("Error fetching product by id: ", error);
       return undefined;
     }
   }
 
-  async submitApplication(firstName: string, lastName: string, email: string, text: string): Promise<void> {
+  private constructPhotoUrl(productId: number): string {
+    return `${this.productsUrl}/${productId}/image/image.jpg`;
+  }
+
+   async submitApplication(firstName: string, lastName: string, email: string, text: string): Promise<void> {
     try {
       const userData = {
         username: email,
@@ -87,29 +57,10 @@ export class ProductService {
         enabled: true
       };
 
-      await this.http.post(this.apiUrl, userData, this.httpOptions).toPromise();
+      await this.http.post(this.createUserUrl, userData).toPromise();
       console.log('User created successfully');
     } catch (error) {
       console.error('Error creating user: ', error);
-    }
-  }
-
-  async getAttachmentUrl(docId: string, attachmentName: string): Promise<SafeUrl> {
-    try {
-      const response = await this.http.get(`${this.url}${docId}/${attachmentName}`, {
-        headers: this.httpOptions.headers,
-        responseType: 'blob'
-      }).toPromise();
-
-      if (response) {
-        const objectURL = URL.createObjectURL(response);
-        return this.sanitizer.bypassSecurityTrustUrl(objectURL);
-      } else {
-        return '';
-      }
-    } catch (error) {
-      console.error('Error fetching attachment: ', error);
-      return '';
     }
   }
 }
