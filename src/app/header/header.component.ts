c272de6b5015ba2573330d6a51316feb3ac29531
Linus Weigand <linusweigand@protonmail.com>
2024-03-05 18:32:45 +0100

    add header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    RouterModule
+  ],
+  template: `
+      <header class="brand-name">
+      <a [routerLink]="['/']">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </a>
+      <button type="button" class="btn btn-secondary" (click)="onCartClick()">
+        <img class="cart-icon" src="/assets/cart.png" alt="cart" aria-hidden="true">
+      </button>
+    </header>
+  `,
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+  onCartClick(): void {
+    this.router.navigate(['/cart']);
+  }
+
+}
