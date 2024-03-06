c3c31b4b5e46198d89c0082427aa2136125da04a
Linus Weigand <linusweigand@protonmail.com>
2024-03-06 14:17:32 +0100

    add nav items, Search bar, login, sign-up button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,27 +5,62 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterModule
-  ],
+  imports: [RouterModule],
   template: `
-      <header class="brand-name">
-      <a [routerLink]="['/']" data-bs-toggle="tooltip"  data-bs-html="true" title="Home">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </a>
-      <button type="button" class="btn btn-secondary" (click)="onCartClick()" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-html="true" title="Cart">
-        <img class="cart-icon" src="/assets/cart.png" alt="cart" aria-hidden="true">
-      </button>
+    <header class="p-3 text-bg-dark">
+      <div class="container-fluid">
+        <div
+          class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
+        >
+          <a
+            [routerLink]="['/']"
+            class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+          >
+            <img
+              class="brand-logo"
+              src="/assets/logo.svg"
+              alt="logo"
+              aria-hidden="true"
+            />
+          </a>
+
+          <ul
+            class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"
+          >
+            <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+            <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+          </ul>
+
+          <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+            <input
+              type="text"
+              class="form-control form-control-dark text-bg-dark"
+              id="floatingInput"
+              placeholder="Search..."
+              aria-label="Search"
+              style="color: white"
+            />
+          </form>
+
+          <div class="text-end">
+            <button type="button" class="btn btn-outline-light me-2">
+              Login
+            </button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+          </div>
+        </div>
+      </div>
     </header>
   `,
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   onCartClick(): void {
     this.router.navigate(['/cart']);
   }
-
 }
