8787bf059c0f6b4ece54621665917c2e007cf913
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 12:05:31 +0200

    add styling options data

---
+++ src/app/data/styling.ts
@@ -0,0 +1,20 @@
+export const styling = [
+  {
+    id: 'css',
+    name: 'CSS',
+    description: 'Cascading Style Sheets.',
+    checked: true,
+  },
+  {
+    id: 'sass',
+    name: 'Sass',
+    description: 'Syntactically Awesome Style Sheets.',
+    checked: false,
+  },
+  {
+    id: 'less',
+    name: 'Less',
+    description: 'Less is More.',
+    checked: false,
+  },
+]
