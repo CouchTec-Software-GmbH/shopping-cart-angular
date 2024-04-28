d0d2052bbfbe76262cfc51d1d10ee1a8dc65594a
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 12:04:50 +0200

    add monitoring data

---
+++ src/app/data/monitoring.ts
@@ -0,0 +1,20 @@
+export const monitoring = [
+  {
+    id: 'prometheus',
+    name: 'Prometheus',
+    description: 'Prometheus.',
+    checked: false,
+  },
+  {
+    id: 'grafana',
+    name: 'Grafana',
+    description: 'Grafana.',
+    checked: false,
+  },
+  {
+    id: 'data-dog',
+    name: 'Data Dog',
+    description: 'Data Dog.',
+    checked: false,
+  }
+]
