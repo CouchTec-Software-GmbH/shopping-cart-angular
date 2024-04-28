ae31234d5577e8a12483275923533b90d6c59d7a
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 13:01:00 +0200

    add summary component

---
+++ src/app/pages/dashboard/components/summary/summary.component.ts
@@ -0,0 +1,30 @@
+import { Component, Output, Input, EventEmitter} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProjectOption } from '@models/project-option';
+import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { security } from '@app/data/security';
+
+@Component({
+  selector: 'app-summary',
+  standalone: true,
+  imports: [CommonModule, CheckboxComponent],
+  template: `
+
+        <h1 class="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+          Summary
+        </h1>
+
+        <p class="mt-4 leading-relaxed text-gray-500">
+          Choose the security options you want to use for your project.
+        </p>
+
+    <app-checkbox [options]="options" (selectionChange)="selectedOptions = $event"></app-checkbox>
+  `,
+})
+export class SummaryComponent {
+  @Output() selectionChange = new EventEmitter<string>();
+
+  options: ProjectOption[] = security;
+
+  selectedOptions: string[] = [];
+}
