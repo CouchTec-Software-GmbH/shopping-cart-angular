2fef23e705f217d286d8add530d0f5364b04c380
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 12:05:07 +0200

    add project structure data

---
+++ src/app/data/project-structure-options.ts
@@ -0,0 +1,21 @@
+export const projectStructureOptions = [
+  {
+    id: 'spa',
+    name: 'Single Page Application',
+    description: 'Single page application.',
+    checked: false,
+  },
+  {
+    id: 'mpa',
+    name: 'Multi Page Application',
+    description: 'Multiple page application.',
+    checked: true,
+  },
+  {
+    id: 'pwa',
+    name: 'Progressive Web Application',
+    description: 'Web application that loads like a regular web page or offline.',
+    checked: false,
+  },
+];
+
