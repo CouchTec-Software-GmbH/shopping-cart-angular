63f38ee58d298842405e7a4348775b2c5326d085
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 12:05:25 +0200

    add steps data

---
+++ src/app/data/steps.ts
@@ -0,0 +1,33 @@
+export const steps = [
+  {
+    id: 'project-type',
+    name: 'Type',
+  },
+  {
+    id: 'technology-stack',
+    name: 'Tech Stack',
+  },
+  {
+    id: 'deployment',
+    name: 'Deployment',
+  },
+  {
+    id: 'security',
+    name: 'Security',
+  },
+  {
+    id: 'monitoring',
+    name: 'Monitoring',
+  },
+  {
+    id: 'extras',
+    name: 'Extras',
+  },
+  {
+    id: 'summary',
+    name: 'Summary',
+  },
+];
+
+
+
