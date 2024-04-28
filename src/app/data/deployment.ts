8461265c0c0ddd0542823da74611764e6869fee2
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 12:04:28 +0200

    add deployment data

---
+++ src/app/data/deployment.ts
@@ -0,0 +1,78 @@
+export const provider = [
+  {
+    id: 'AWS',
+    name: 'AWS',
+    description: 'AWS.',
+    checked: true,
+  },
+  {
+    id: 'Azure',
+    name: 'Azure',
+    description: 'Azure.',
+    checked: false,
+  },
+  {
+    id: 'internal',
+    name: 'Internal',
+    description: 'Internal.',
+    checked: false,
+  },
+]
+export const containerization = [
+  {
+    id: 'Docker',
+    name: 'Docker',
+    description: 'Docker.',
+    checked: true,
+  },
+  {
+    id: 'Podman',
+    name: 'Podman',
+    description: 'Podman.',
+    checked: false,
+  },
+]
+
+export const orchestration = [
+  {
+    id: 'Kubernetes',
+    name: 'Kubernetes',
+    description: 'Kubernetes.',
+    checked: true,
+  },
+  {
+    id: 'Docker Swarm',
+    name: 'Docker Swarm',
+    description: 'Docker Swarm.',
+    checked: false,
+  },
+  {
+    id: 'OpenShift',
+    name: 'OpenShift',
+    description: 'OpenShift.',
+    checked: false,
+  },
+]
+
+
+export const environment = [
+  {
+    id: 'dev',
+    name: 'Development',
+    description: 'Development.',
+    checked: true,
+  },
+  {
+    id: 'staging',
+    name: 'Staging',
+    description: 'Staging.',
+    checked: false,
+  },
+  {
+    id: 'prod',
+    name: 'Production',
+    description: 'Production.',
+    checked: false,
+  },
+]
+
