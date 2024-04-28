b8649c1855510f39fc67bac2534bebc18818e360
Linus Weigand <linusweigand@protonmail.com>
2024-04-28 12:04:59 +0200

    add project type option data

---
+++ src/app/data/project-options.ts
@@ -0,0 +1,38 @@
+  export const projectOptions = [
+    {
+      id: 'GreenFieldProject',
+      name: 'Green Field Project',
+      description: 'Starting from scratch.',
+      checked: true,
+    },
+    {
+      id: 'BrownFieldProject',
+      name: 'Brown Field Project',
+      description: 'Modifying or integrating with existing systems.',
+      checked: false,
+    },
+    {
+      id: 'GrayFieldProject',
+      name: 'Gray Field Project',
+      description: 'Starting from a template or existing project',
+      checked: false,
+    },
+    {
+      id: 'InfrastructureProject',
+      name: 'Infrastructure Project',
+      description: 'Focused on setting up or upgrading infrastructure.',
+      checked: false,
+    },
+    {
+      id: 'MigrationProject',
+      name: 'Migration Project',
+      description: 'Moving from one environment or technology to another.',
+      checked: false,
+    },
+    {
+      id: 'IntegrationProject',
+      name: 'Integration Project',
+      description: 'Connecting multiple systems or technologies.',
+      checked: false,
+    },
+  ];
