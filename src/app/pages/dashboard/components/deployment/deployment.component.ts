fc53c76b4abd9f69bbf05135ac15825a43ce7cea
Linus Weigand <linusweigand@protonmail.com>
2024-04-29 16:45:33 +0200

    deployment comp: remove Input, Output, add localStorage

--- src/app/pages/dashboard/components/deployment/deployment.component.ts
+++ src/app/pages/dashboard/components/deployment/deployment.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectOption } from '@models/project-option';
 import { RadioComponent } from '../radio/radio.component';
-import { containerization, environment, orchestration, provider } from '@app/data/deployment';
 import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { containerizationOptions, environmentOptions, orchestrationOptions, providerOptions } from '@app/data/deployment';
 
 @Component({
   selector: 'app-deployment',
@@ -18,59 +18,83 @@ import { CheckboxComponent } from '../checkbox/checkbox.component';
         <p class="mt-4 leading-relaxed text-gray-500">
           Choose the provider.
         </p>
-        <app-radio [options]="provider" (providerOutput)="handleProviderSelectionChange($event)"></app-radio>
+        <app-radio [options]="provider" (selectionChange)="handleProviderSelectionChange($event)"></app-radio>
 
 
       <p class="mt-4 leading-relaxed text-gray-500">
         Choose the containerization.
       </p>
-      <app-radio [options]="containerization" (containerizationOutput)="handleContainerizationSelectionChange($event)"></app-radio>
+      <app-radio [options]="containerization" (selectionChange)="handleContainerizationSelectionChange($event)"></app-radio>
 
       <p class="mt-4 leading-relaxed text-gray-500">
         Choose the orchestration.
       </p>
-      <app-radio [options]="orchestration" (orchestrationOutput)="handleOrchestrationSelectionChange($event)"></app-radio>
+      <app-radio [options]="orchestration" (selectionChange)="handleOrchestrationSelectionChange($event)"></app-radio>
 
       <p class="mt-4 leading-relaxed text-gray-500">
         Choose the environment.
       </p>
-      <app-checkbox [options]="environment" (environmentOutput)="handleEnvironmentSelectionChange($event)"></app-checkbox>
+      <app-checkbox [options]="environment" (selectionChange)="handleEnvironmentSelectionChange($event)"></app-checkbox>
   `,
 })
 export class DeploymentComponent {
-  @Input() options: ProjectOption[] = [];
-  @Output() providerOutput = new EventEmitter<string>();
-  @Output() containerizationOutput = new EventEmitter<string>();
-  @Output() orchestrationOutput = new EventEmitter<string>();
-  @Output() environmentOutput = new EventEmitter<string[]>();
+  provider: ProjectOption[] = providerOptions;
+  containerization: ProjectOption[] = containerizationOptions;
+  orchestration: ProjectOption[] = orchestrationOptions;
+  environment: ProjectOption[] = environmentOptions;
 
-  provider: ProjectOption[] = provider;
-  containerization: ProjectOption[] = containerization;
-  orchestration: ProjectOption[] = orchestration;
-  environment: ProjectOption[] = environment;
-
-  selectedProvider: string = '';
-  selectedContainerization: string = '';
-  selectedOrchestration: string = '';
-  selectedEnvironments: string[] = [];
+  constructor() {
+    const provider = localStorage.getItem('provider');
+    if (provider) {
+      this.provider = this.provider.map(option => {
+        option.checked = option.id === provider;
+        return option;
+      });
+    } else {
+      localStorage.setItem('provider', this.provider.filter(option => option.checked).map(option => option.id)[0]);
+    }
+    const containerization = localStorage.getItem('containerization');
+    if (containerization) {
+      this.containerization = this.containerization.map(option => {
+        option.checked = option.id === containerization;
+        return option;
+      });
+    } else {
+      localStorage.setItem('containerization', this.containerization.filter(option => option.checked).map(option => option.id)[0]);
+    }
+    const orchestration = localStorage.getItem('orchestration');
+    if (orchestration) {
+      this.orchestration = this.orchestration.map(option => {
+        option.checked = option.id === orchestration;
+        return option;
+      });
+    } else {
+      localStorage.setItem('orchestration', this.orchestration.filter(option => option.checked).map(option => option.id)[0]);
+    }
+    const environment = localStorage.getItem('environment');
+    if (environment) {
+      this.environment = this.environment.map(option => {
+        option.checked = environment.includes(option.id);
+        return option;
+      });
+    } else {
+      localStorage.setItem('environment', JSON.stringify(this.environment.filter(option => option.checked).map(option => option.id)));
+    }
+  }
 
   handleProviderSelectionChange(selectionId: string): void {
-    this.selectedProvider = selectionId;
-    this.providerOutput.emit(selectionId);
+    localStorage.setItem('provider', selectionId);
   }
 
   handleContainerizationSelectionChange(selectionId: string): void {
-    this.selectedContainerization = selectionId;
-    this.containerizationOutput.emit(selectionId);
+    localStorage.setItem('containerization', selectionId);
   }
 
   handleOrchestrationSelectionChange(selectionId: string): void {
-    this.selectedOrchestration = selectionId;
-    this.orchestrationOutput.emit(selectionId);
+    localStorage.setItem('orchestration', selectionId);
   }
 
   handleEnvironmentSelectionChange(selectionId: string[]): void {
-    this.selectedEnvironments = selectionId;
-    this.environmentOutput.emit(selectionId);
+    localStorage.setItem('environment', JSON.stringify(selectionId));
   }
 }
