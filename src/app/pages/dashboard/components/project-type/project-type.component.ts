d50589ce40f80b583f8733fc66f5562c602aa3cc
Linus Weigand <linusweigand@protonmail.com>
2024-04-29 09:18:09 +0200

    project type comp: emit selected options

--- src/app/pages/dashboard/components/project-type/project-type.component.ts
+++ src/app/pages/dashboard/components/project-type/project-type.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectOption } from '@models/project-option';
 import { RadioComponent } from '../radio/radio.component';
+import { projectOptions } from '@app/data/project-options';
 
 @Component({
   selector: 'app-project-type',
   standalone: true,
   imports: [CommonModule, RadioComponent],
   template: `
-
         <h1 class="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
           Start configuring your project
         </h1>
@@ -16,18 +16,15 @@ import { RadioComponent } from '../radio/radio.component';
         <p class="mt-4 leading-relaxed text-gray-500">
           Choose the type of project you want to develop / expand.
         </p>
-
-  <app-radio [options]="options" (selectionChange)="selectedOption = $event"></app-radio>
+        <app-radio [options]="options" (selectionChange)="onSelectionChange($event)"></app-radio>
   `,
 })
 export class ProjectTypeComponent {
-  @Input() options: ProjectOption[] = [];
   @Output() selectionChange = new EventEmitter<string>();
-
-  selectedOption: string = '';
+  options: ProjectOption[] = projectOptions;
 
   onSelectionChange(selectionId: string): void {
-    this.selectedOption = selectionId;
     this.selectionChange.emit(selectionId);
+    console.log(selectionId);
   }
 }
