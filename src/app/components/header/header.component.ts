341eb5e881cf429ca24add5b307aa76d141f6e7f
Linus Weigand <linusweigand@protonmail.com>
2024-05-01 17:09:25 +0200

    fix login button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -50,9 +50,8 @@ import { CommonModule } from '@angular/common';
             <div class="sm:flex sm:gap-4">
               <a
                 class="block rounded-md bg-blue-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-700"
-                href="#"
               >
-                Login
+                  [routerLink]="['/login']"
               </a>
 
               <a
