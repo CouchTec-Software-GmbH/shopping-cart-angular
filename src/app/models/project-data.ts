4f42e3b08ba9a958559402e0fa8e81ef9bb43fef
Linus Weigand <linusweigand@protonmail.com>
2024-05-18 16:16:55 +0200

    add projectData interface model

---
+++ src/app/models/project-data.ts
@@ -0,0 +1,24 @@
+export interface ProjectData {
+  projectType: string;
+  tierOptions: string[];
+  techStack: {
+    frontend: {
+      framework: string;
+      styling: string;
+    };
+    middleware: string;
+    backend: string;
+    database: string;
+  };
+  deployment: {
+    provider: string;
+    containerization: string;
+    orchestration: string;
+    environment: string[];
+  };
+  security: string[];
+  monitoring: string;
+  extra: string[];
+}
+
+
