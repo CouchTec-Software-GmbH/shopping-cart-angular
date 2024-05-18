cfc2547522fdb103bea3a0eee17329c6249d1ee7
Linus Weigand <linusweigand@protonmail.com>
2024-05-18 16:19:26 +0200

    summary: localStorage -> couchdb

--- src/app/pages/dashboard/components/summary/summary.component.ts
+++ src/app/pages/dashboard/components/summary/summary.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CheckboxComponent } from '../checkbox/checkbox.component';
+import { CheckboxComponent } from '@components/checkbox/checkbox.component';
+import { createDefaultProjectData } from '@app/utils/utils';
 
 @Component({
   selector: 'app-summary',
@@ -11,77 +12,80 @@ import { CheckboxComponent } from '../checkbox/checkbox.component';
         <h1 class="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
           Summary
         </h1>
+         <p class="mt-4 leading-relaxed text-gray-500">
+            Project ID: {{ this.uuid }}
+        </p>
 
         <div class="flow-root mt-4">
   <dl class="-my-3 divide-y divide-gray-100 text-sm">
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Project Type</dt>
-      <dd class="text-gray-700 sm:col-span-2"> {{ projectTypeSelection  }} </dd>
+      <dd class="text-gray-700 sm:col-span-2"> {{ projectData.projectType  }} </dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Tiers</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ tierSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.tierOptions }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Frontend Framework</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ frontendFrameworkSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.techStack.frontend.framework }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Styling</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ stylingSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.techStack.frontend.styling }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Middleware</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ middlewareSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.techStack.middleware }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Backend</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ backendSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.techStack.backend }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Database</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ databaseSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.techStack.database }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Provider</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ providerSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.deployment.provider }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Containerization</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ containerizationSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.deployment.containerization }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Orchestration</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ orchestrationSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.deployment.orchestration }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Environments</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ environmentSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.deployment.environment }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Security</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ securitySelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.security }}</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Monitoring</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ monitoringSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.monitoring }}</dd>
     </div>
     <div class="grid grid-cols-1 gap-1 py-3 even:bg-gray-50 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Additional Features</dt>
-      <dd class="text-gray-700 sm:col-span-2">{{ additionalFeaturesSelection }}</dd>
+      <dd class="text-gray-700 sm:col-span-2">{{ projectData.extra }}</dd>
     </div>
   </dl>
 </div>
@@ -90,44 +94,6 @@ import { CheckboxComponent } from '../checkbox/checkbox.component';
   `,
 })
 export class SummaryComponent {
-
-  projectTypeSelection: string = localStorage.getItem('projectType') || '';
-  tierSelection: string[] = localStorage.getItem('tiers') ? JSON.parse(localStorage.getItem('tiers') || '') : [];
-  frontendFrameworkSelection: string = localStorage.getItem('frontendFramework') || '';
-  stylingSelection: string = localStorage.getItem('styling') || '';
-  middlewareSelection: string = localStorage.getItem('middleware') || '';
-  backendSelection: string = localStorage.getItem('backend') || '';
-  databaseSelection: string = localStorage.getItem('database') || '';
-  providerSelection: string = localStorage.getItem('provider') || '';
-  containerizationSelection: string = localStorage.getItem('containerization') || '';
-  orchestrationSelection: string = localStorage.getItem('orchestration') || '';
-  environmentSelection: string[] = localStorage.getItem('environment') ? JSON.parse(localStorage.getItem('environment') || '') : [];
-  securitySelection: string[] = localStorage.getItem('security') ? JSON.parse(localStorage.getItem('security') || '') : [];
-  monitoringSelection: string = localStorage.getItem('monitoring') || '';
-  additionalFeaturesSelection: string[] = localStorage.getItem('extra') ? JSON.parse(localStorage.getItem('extra') || '') : [];
-
-  constructor() {
-    const tiers = localStorage.getItem('tiers');
-    if (tiers) {
-      console.log(tiers);
-      this.tierSelection = JSON.parse(tiers);
-      console.log(this.tierSelection);
-    }
-    const environment = localStorage.getItem('environment');
-    if (environment) {
-      console.log(environment);
-      this.environmentSelection = JSON.parse(environment);
-      console.log(this.environmentSelection);
-    }
-    const security = localStorage.getItem('security');
-    if (security) {
-      console.log(security);
-      this.securitySelection = JSON.parse(security);
-      console.log(this.securitySelection);
-    }
-    const additionalFeatures = localStorage.getItem('extra');
-    if (additionalFeatures) {
-      this.additionalFeaturesSelection = JSON.parse(additionalFeatures);
-    }
-  }
+  @Input() uuid: string = localStorage.getItem('uuid') || '';
+  @Input() projectData = createDefaultProjectData();
 }
