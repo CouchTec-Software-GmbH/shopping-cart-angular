00ef25f5feeb82878d07acb9295bbf90e2517aa6
Linus Weigand <linusweigand@protonmail.com>
2024-06-10 12:46:39 +0200

    add "New Project button"

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -57,24 +57,39 @@ import { Subscription } from 'rxjs';
 
 <div class="relative">
   <div class="inline-flex items-center overflow-hidden ">
-    <button
+
+
+    <button *ngIf="name === ''"
   (click)="onAccountClick()"
   class="group inline-block rounded-full hover:bg-gray-100 transition-colors duration-200"
 >
   <span
     class="block rounded-full bg-white px-3 py-3 text-sm group-hover:bg-transparent"
   >
-    <div *ngIf="name === ''">
+
+
     <svg fill="currentColor" width="20px" height="20px" viewBox="0 0 128 128" id="Layer_1" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 <g>
 <path d="M30,49c0,18.7,15.3,34,34,34s34-15.3,34-34S82.7,15,64,15S30,30.3,30,49z M90,49c0,14.3-11.7,26-26,26S38,63.3,38,49   s11.7-26,26-26S90,34.7,90,49z"/>
 <path d="M24.4,119.4C35,108.8,49,103,64,103s29,5.8,39.6,16.4l5.7-5.7C97.2,101.7,81.1,95,64,95s-33.2,6.7-45.3,18.7L24.4,119.4z"/>
 </g>
 </svg>
-</div>
-  <p *ngIf="name !== ''" class="text-gray-500">{{ name }}</p>
   </span>
 </button>
+
+<a *ngIf="name !== ''"
+    class="block rounded-full bg-white px-3 py-3 text-sm group-hover:bg-transparent hover:underline hover:cursor-pointer"
+    (click)="onAccountClick()"
+>
+  <span
+    class="block rounded-full bg-white px-3 py-3 text-sm group-hover:bg-transparent"
+  >
+  <p class="text-gray-500">{{ name }}</p>
+  </span>
+</a>
+
+
+
   </div>
 
   <div
@@ -91,12 +106,29 @@ import { Subscription } from 'rxjs';
         *ngFor="let uuid of uuids"
         class="block rounded-lg px-4 py-2 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
         role="menuitem"
+        (click)=onProjectClick(uuid)
+        [ngClass]="{ 'font-bold': currentUuid === uuid}"
       >
         {{uuid}}
       </a>
 
           </div>
   <div class="p-2" *ngIf="name !== ''">
+
+     <button
+          (click)="onNewProjectClick()"
+          class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-blue-600 hover:bg-blue-50"
+          role="menuitem"
+        >
+<svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+<rect x="0" fill="none" width="24" height="24"/>
+<g>
+<path d="M21 14v5c0 1.105-.895 2-2 2H5c-1.105 0-2-.895-2-2V5c0-1.105.895-2 2-2h5v2H5v14h14v-5h2z"/>
+<path d="M21 7h-4V3h-2v4h-4v2h4v4h2V9h4"/>
+</g>
+</svg>
+          New Project
+        </button>
      <button
           (click)="onSignOutClick()"
           class="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-red-600 hover:bg-red-50"
@@ -184,6 +216,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
   authService = inject(AuthService);
   private authSubscription!: Subscription;
   uuids: string[] = [];
+  currentUuid: string = "";
 
   constructor(
     private router: Router,
@@ -196,6 +229,9 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.authSubscription = this.authService.isAuthenticated$.subscribe(async isAuthenticated => {
       this.updateEmailFromCookies();
       this.uuids = await this.authService.getUuids();
+      if ( this.uuids.length > 0 ) {
+        this.currentUuid = this.uuids[0];
+      }
       console.log("Uuids:", this.uuids);
     });
   }
@@ -204,6 +240,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
+  }
+
+  onProjectClick(uuid: string): void {
+    localStorage.setItem('uuid', uuid);
+    this.currentUuid = uuid;
+
+    window.dispatchEvent(new StorageEvent('storage', {
+      key: 'uuid',
+      newValue: uuid
+    }));
+
+    this.router.navigate(['dashboard']);
   }
 
   onSearch(value: string): void {
@@ -225,6 +273,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
   onSignOutClick(): void {
     this.authService.signOut();
     this.accountShow = false;
+  }
+
+  onNewProjectClick(): void {
+    let new_uuid = crypto.randomUUID();
+    this.uuids.push(new_uuid);
+    this.authService.addUuid(new_uuid);
+    this.onProjectClick(new_uuid);
   }
 
   private updateEmailFromCookies() {
