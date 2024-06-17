92afd4062d4e961bce11ebc1c4544acdd0bfb337
Linus Weigand <linusweigand@protonmail.com>
2024-06-17 16:46:01 +0200

    remove old api code

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -53,58 +53,6 @@ export class ProductService {
     }
   }
 
-  async getAllProducts(): Promise<Product[]> {
-    try {
-      const response = await firstValueFrom(this.http.get<any>(`${this.url}_all_docs?include_docs=true`, this.httpOptions));
-
-      const productPromises = response.rows.map(async (row: any, index: number) => {
-        const doc = row.doc;
-        let photoUrl = null;
-        if (doc._attachments) {
-          photoUrl = await this.getAttachmentUrl(doc._id, Object.keys(doc._attachments)[0]);
-        } else  {
-          photoUrl = null;
-        }
-        return {
-          id: index,
-          name: doc.name,
-          price: doc.price,
-          photo: photoUrl,
-          description: doc.description
-        } as Product;
-      });
-
-      return await Promise.all(productPromises);
-    } catch (error) {
-      console.error("Error fetching products: ", error);
-      return [];
-    }
-  }
-  async getProductById(id: string): Promise<Product | undefined> {
-    try {
-      const response = await firstValueFrom(this.http.get<any>(`${this.url}${id}`, this.httpOptions));
-
-      if (!response) {
-        return undefined;
-      }
-
-      let photoUrl: string| SafeUrl = '';
-      if (response._attachments) {
-        photoUrl = await this.getAttachmentUrl(response._id, Object.keys(response._attachments)[0]);
-      }
-
-      return {
-        id: parseInt(id),
-        name: response.name,
-        price: response.price,
-        photo: photoUrl,
-        description: response.description
-      } as Product;
-    } catch (error) {
-      console.error("Error fetching product by id: ", error);
-      return undefined;
-    }
-  }
 
   async submitApplication(firstName: string, lastName: string, email: string, text: string): Promise<void> {
     try {
@@ -120,25 +68,6 @@ export class ProductService {
       console.log('User created successfully');
     } catch (error) {
       console.error('Error creating user: ', error);
-    }
-  }
-
-  async getAttachmentUrl(docId: string, attachmentName: string): Promise<SafeUrl> {
-    try {
-      const response = await firstValueFrom(this.http.get(`${this.url}${docId}/${attachmentName}`, {
-        headers: this.httpOptions.headers,
-        responseType: 'blob'
-      }));
-
-      if (response) {
-        const objectURL = URL.createObjectURL(response);
-        return this.sanitizer.bypassSecurityTrustUrl(objectURL);
-      } else {
-        return '';
-      }
-    } catch (error) {
-      console.error('Error fetching attachment: ', error);
-      return '';
     }
   }
 }
