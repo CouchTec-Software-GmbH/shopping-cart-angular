088622305b8940954c8632e7f4dbc49d58de761d
Linus Weigand <linusweigand@protonmail.com>
2024-06-17 12:11:07 +0200

    remove unneccessary side menu options in settings

--- src/app/pages/settings/components/sidemenu/sidemenu.component.ts
+++ src/app/pages/settings/components/sidemenu/sidemenu.component.ts
@@ -11,68 +11,11 @@ import { RouterModule } from '@angular/router';
     CommonModule
   ],
   template: `
-<div class="flex h-screen flex-col justify-between border-e bg-white px-8">
+<div class="flex h-screen flex-col justify-between border-e bg-white ">
   <div class="px-0 py-6">
 
 
-    <ul class="mt-6 space-y-1">
-      <li>
-        <a
-          (click)="onStateChange('general')"
-          class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 hover:cursor-pointer"
-
-            [ngClass]="{ 'bg-gray-100': state === 'general' }"
-        >
-          General
-        </a>
-      </li>
-
-      <li>
-        <details class="group [&_summary::-webkit-details-marker]:hidden">
-          <summary
-            class="flex cursor-pointer items-center justify-between rounded-lg px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700"
-          >
-            <span class="text-sm font-medium"> Admin </span>
-
-            <span class="shrink-0 transition duration-300 group-open:-rotate-180">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </span>
-          </summary>
-
-          <ul class="mt-2 space-y-1 px-4">
-            <li>
-              <a
-                (click)="onStateChange('user')"
-            [ngClass]="{ 'bg-gray-100': state === 'user' }"
-                class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:cursor-pointer"
-              >
-                Users
-              </a>
-            </li>
-
-            <li>
-              <a
-                (click)="onStateChange('roles')"
-            [ngClass]="{ 'bg-gray-100': state === 'roles' }"
-                class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 hover:cursor-pointer"
-              >
-                Roles
-              </a>
-            </li>
-          </ul>
-        </details>
-      </li>
+    <ul class="mt-6 space-y-1 px-8">
 
       <li>
         <a
@@ -116,7 +59,7 @@ import { RouterModule } from '@angular/router';
 export class SideMenuComponent implements OnInit {
   email: string = "";
   name: string = "";
-  @Input() state: string = "general";
+  @Input() state: string = "projects";
   @Output() stateChange = new EventEmitter<string>();
 
   ngOnInit(): void {
