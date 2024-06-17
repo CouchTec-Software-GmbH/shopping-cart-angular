9a1141f28b9709f57ede550f972f129841ae027f
Linus Weigand <linusweigand@protonmail.com>
2024-06-17 16:48:44 +0200

    add session token to header in project service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,33 +4,23 @@ import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { firstValueFrom } from 'rxjs';
 import { ProjectData } from '@models/project-data';
-import { createDefaultProjectData } from '@utils/utils';
+import { createDefaultProjectData, get_http_header, get_session_token_from_cookie } from '@utils/utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  url = 'https://couchdb-app-service.azurewebsites.net/products/';
   // apiUrl = `https://couchtec.dev.linusweigand.com/api/`;
   apiUrl = `http://localhost/api/`;
-  private httpOptions: { headers: HttpHeaders };
 
 
-  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
-    const username = 'admin';
-    const password = '8RzuxhQ7';
-
-    this.httpOptions = {
-      headers: new HttpHeaders({
-        'Authorization': 'Basic ' + btoa(username + ':' + password),
-        'Content-Type': 'application/json'
-      })
-    };
+  constructor(private http: HttpClient) {
   }
 
   async putProject(uuid: string, body: Record<string, any>): Promise<void> {
     try {
-      await firstValueFrom(this.http.put(`${this.apiUrl}${uuid}`, body, this.httpOptions));
+      let session_token = get_session_token_from_cookie();
+      await firstValueFrom(this.http.put(`${this.apiUrl}${uuid}`, body, get_http_header(session_token ?? '')));
     } catch (error) {
       // if 404 put default project data
       if (error instanceof HttpErrorResponse && error.status === 404) {
@@ -43,7 +33,8 @@ export class ProductService {
 
   async getProject(uuid: string): Promise<ProjectData> {
     try {
-      const response = await firstValueFrom(this.http.get(`${this.apiUrl}${uuid}`, this.httpOptions));
+      let session_token = get_session_token_from_cookie();
+      const response = await firstValueFrom(this.http.get(`${this.apiUrl}${uuid}`, get_http_header(session_token ?? '')));
       return {...createDefaultProjectData(), ...response};
     } catch (error) {
       console.log("No project with that uuid found");
