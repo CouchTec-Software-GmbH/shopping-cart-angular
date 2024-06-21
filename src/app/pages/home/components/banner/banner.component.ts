ed9f6aec331afb4be8a756f8bce31bd35165b109
Linus Weigand <linusweigand@protonmail.com>
2024-06-21 12:54:40 +0200

    translate banner to german

--- src/app/pages/home/components/banner/banner.component.ts
+++ src/app/pages/home/components/banner/banner.component.ts
@@ -12,14 +12,14 @@ import { RouterModule } from '@angular/router';
       >
         <div class="mx-auto max-w-xl text-center">
           <h1 class="text-7xl font-extrabold sm:text-7xl">
-            Welcome at
+            CouchTec
             <strong class="font-extrabold text-blue-700 sm:block">
-              CouchTec
+              Kundenportal
             </strong>
           </h1>
 
           <p class="mt-4 sm:text-2xl/relaxed">
-            Individual Web Application Development
+            Konfigurieren Sie Ihre Projekte
           </p>
 
           <div class="mt-8 flex flex-wrap justify-center gap-4">
@@ -27,13 +27,13 @@ import { RouterModule } from '@angular/router';
               class="block w-full rounded bg-blue-600 px-12 py-3 text-sm text-white font-medium  shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
           [routerLink]="['/dashboard']"
             >
-              Get Started
+              Starten
             </a>
             <a
               class="block w-full rounded px-12 py-3 text-sm font-medium text-blue-600 shadow hover:text-blue-700 focus:outline-none focus:ring active:text-blue-500 sm:w-auto"
 
             >
-              Learn More
+              Mehr erfahren
             </a>
           </div>
         </div>
