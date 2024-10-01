fa2baaa336af53375d627f733cb901a445e8abd1
Linus Weigand <linusweigand@protonmail.com>
2024-10-01 23:22:52 +0200

    add new tech stack section

---
+++ src/app/pages/home/components/stack-section/stack-section.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+    selector: 'app-stack-section',
+    standalone: true,
+    imports: [
+        RouterModule,
+    ],
+    templateUrl: './stack-section.component.html'
+})
+export class StackSection {
+}
