0561a741fab415328171bba42e08099d17d2aa1e
Linus Weigand <linusweigand@protonmail.com>
2024-10-01 23:22:20 +0200

    add new contact section landing page

---
+++ src/app/pages/home/components/contact-section/contact-section.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+    selector: 'app-contact-section',
+    standalone: true,
+    imports: [
+        RouterModule,
+    ],
+    templateUrl: './contact-section.component.html'
+})
+export class ContactSection {
+}
