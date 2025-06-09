c08bfc66756948f9f7d5ec12628be5df462f80ef
Linus Weigand <linusweigand@protonmailcom>
2025-06-09 19:37:54 +0200

    format

--- src/app/pages/heymano/heymano.component.ts
+++ src/app/pages/heymano/heymano.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavigationService } from '@app/services/navigation.service';
+import { HeaderComponent } from '../home/components/header/header.component';
+import { FooterComponent } from '../home/components/footer/footer.component';
 
 @Component({
   selector: 'app-heymano',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './heymano.component.html',
 })
 export class HeyManoComponent {
